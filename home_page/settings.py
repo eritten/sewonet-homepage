07724e7d42e646339482c13c521c9111870cf08b
= <=>
2023-04-20 03:43:49 -0200

    adding mysql settings

--- home_page/settings.py
+++ home_page/settings.py
@@ -88,8 +88,12 @@ import os
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "serwornet",
+"USER": "serwornet",
+"PASSWORD": "serwornet1234$1234",
+"HOST": "swonet.mysql.pythonanywhere-services.com",
+
     }
 }
 
